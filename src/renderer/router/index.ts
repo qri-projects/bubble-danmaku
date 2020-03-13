c6d5ea7334103198e6c415bd5f36ba3234140fb2
hhhhhg <va.qri@qq.com>
2020-03-14 03:44:10 +0800

    router

--- src/renderer/router/index.ts
+++ src/renderer/router/index.ts
@@ -5,13 +5,18 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
-    },
+    // {
+    //   path: '/',
+    //   name: 'landing-page',
+    //   component: require('@/components/LandingPage').default
+    // },
     {
       path: '/danmaku',
+      name: 'danmaku_1',
+      component: require('@/components/DanmakuPage').default
+    },
+    {
+      path: '/',
       name: 'danmaku',
       component: require('@/components/DanmakuPage').default
     },
