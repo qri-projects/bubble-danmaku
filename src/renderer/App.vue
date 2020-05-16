5a30a37393b780afe8bf92eef513ec091530262d
hhhhhg <va.qri@qq.com>
2020-05-17 04:21:15 +0800

    初始化

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -8,6 +8,17 @@
     import {handleConfigLoaded} from "./scripts/configLoaded"
     import electron from "electron";
     import Vue from 'vue';
+    import store from "./store";
+
+    const emptyFunction = (...args) => {};
+    if (!store.state.dev) {
+        window.console.debug = emptyFunction;
+        window.console.error = emptyFunction;
+        window.console.info = emptyFunction;
+        window.console.log = emptyFunction;
+        window.console.warn = emptyFunction;
+        window.console.dir = emptyFunction;
+    }
 
     export default Vue.extend({
         name: 'bili-danmaku-electron-vue',
